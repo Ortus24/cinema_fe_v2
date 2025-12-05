67523ef7aada3bbbcb1a09ed492b0cebe1017073
Ortus24 <nhatlckbt007@gmail.com>
2025-12-05 10:51:41 +0700

    fix part properti

--- src/components/Home/Cinema/index.tsx
+++ src/components/Home/Cinema/index.tsx
@@ -26,7 +26,7 @@ const CinemaListing: React.FC = () => {
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const res = await fetch(process.env.BACKEND_URL + "/movie");
+        const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL + "/movie");
         const data = await res.json();
         setMovies(data);
         setFilteredMovies(data);
